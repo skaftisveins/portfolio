9fd5e277ebd1fcb4b9c52ebf22bb4a6599c3f6c8
Skafti Sveinsson <skaftisveins@gmail.com>
2022-10-27 01:04:58 +0000

    initial

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Skafti's Portfolio</title>
+      </Head>
+
+      <h1 className='text-blue-700'>We got this!!</h1>
+
+    </div>
+  );
+};
+
+export default Home
