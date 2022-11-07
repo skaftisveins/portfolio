ff495a378f1fb217335cb0a662d169282c30a36d
Skafti Sveinsson <skafti@tactica.is>
2022-11-07 15:34:17 +0000

    Working on work experience section

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 function ExperienceCard({}: Props) {
   return (
-    <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929]p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
+    <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
       <motion.img
         initial={{
           y: -100,
@@ -19,8 +19,8 @@ function ExperienceCard({}: Props) {
         alt=''
       />
       <div className='px-0 md:px-10'>
-        <h4 className='text-4xl font-light'></h4>
-        BLeh<p className='font-bold text-2xl mt-1'>more bleh</p>
+        <h4 className='text-4xl font-light'>BLeh</h4>
+        <p className='font-bold text-2xl mt-1'>more bleh</p>
         <div className='flex space-x-2 my-2'>
           <img
             className='h-10 w-10 rounded-full'
