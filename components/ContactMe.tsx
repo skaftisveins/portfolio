23b4b7db84a46eee634eb0c0bd39e9f877ac8eb1
Skafti Sveinsson <skaftisveins@gmail.com>
2022-11-08 22:35:19 +0000

    Forms section added

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { PhoneIcon, MapPinIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
+import { useForm, Resolver, SubmitHandler } from "react-hook-form";
 
+type FormValues = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
 type Props = {};
 
 const ContactMe = ({}: Props) => {
+  const { register, handleSubmit } = useForm<FormValues>();
+  const onSubmit: SubmitHandler<FormValues> = (formData) =>
+    (window.location.href = `mailto:skaftisveins@gmail?subject=${formData.subject}&body=Hello, you got mail from ${formData.email}. ${formData.message}(${formData.email})`);
   return (
     <div className="h-screen flex relative flex-col text-center md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-[#008ad8] text-2xl">
@@ -30,13 +40,35 @@ const ContactMe = ({}: Props) => {
             <p className="text-2xl">123 Developer Lane</p>
           </div>
         </div>
-        <form className="flex flex-col space-y-2 mx-auto">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-2 mx-auto"
+        >
           <div className="flex space-x-2">
-            <input placeholder="Name" className="contactInput" type="text" />
-            <input placeholder="Email" className="contactInput" type="Email" />
+            <input
+              {...register("name")}
+              placeholder="Name"
+              className="contactInput"
+              type="text"
+            />
+            <input
+              {...register("email")}
+              placeholder="Email"
+              className="contactInput"
+              type="Email"
+            />
           </div>
-          <input placeholder="Subject" className="contactInput" type="text" />
-          <textarea placeholder="Message" className="contactInput" />
+          <input
+            {...register("subject")}
+            placeholder="Subject"
+            className="contactInput"
+            type="text"
+          />
+          <textarea
+            {...register("message")}
+            placeholder="Message"
+            className="contactInput"
+          />
           <button
             type="submit"
             className="submitInput bg-[#008ad8] py-5 px-10 rounded-md text-black font-bold"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,43 @@
-import { NextPage } from 'next';
-import Head from 'next/head';
-import About from '../components/About';
-import ContactMe from '../components/ContactMe';
-import Header from '../components/Header';
-import Projects from '../components/Projects';
-import Skills from '../components/Skills';
-import WorkExperience from '../components/WorkExperience';
-import Hero from './../components/Hero';
+import { NextPage } from "next";
+import Head from "next/head";
+import About from "../components/About";
+import ContactMe from "../components/ContactMe";
+import Header from "../components/Header";
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
+import WorkExperience from "../components/WorkExperience";
+import Hero from "./../components/Hero";
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y overflow-scroll z-0'>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y overflow-y-scroll overflow-x-hidden z-0">
       <Head>
         <title>Skafti&apos;s Portfolio</title>
       </Head>
 
       <Header />
 
-      <section id='hero' className='snap-start'>
+      <section id="hero" className="snap-start">
         <Hero />
       </section>
 
-      <section id='about' className='snap-center'>
+      <section id="about" className="snap-center">
         <About />
       </section>
 
-      <section id='experience' className='snap-center'>
+      <section id="experience" className="snap-center">
         <WorkExperience />
       </section>
 
-      <section id='skills' className='snap-start'>
+      <section id="skills" className="snap-start">
         <Skills />
       </section>
 
-      <section id='projects' className='snap-start'>
+      <section id="projects" className="snap-start">
         <Projects />
       </section>
 
-      <section id='contact' className='snap-start'>
+      <section id="contact" className="snap-start">
         <ContactMe />
       </section>
     </div>
